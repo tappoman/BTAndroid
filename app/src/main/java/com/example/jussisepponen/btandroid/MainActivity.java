f32a189708de50c623fc8a54e5569229d91f32a5
Jussi Sepponen <jussi.sepponen@rdvelho.com>
2018-07-24 12:32:38 +0300

    testing
    initial testing

---
+++ app/src/main/java/com/example/jussisepponen/btandroid/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.jussisepponen.btandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.i("MainActivity", "testing");
+    }
+
+    public void testing(){
+        Log.i("Testing", "function testing");
+    }
+
+}
