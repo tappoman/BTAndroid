b6b2df2d79e972518da1af1e6304152a38bb43f6
Jussi Sepponen <jussi.sepponen@rdvelho.com>
2018-07-24 13:29:20 +0300

    more more testing

--- btandroid/src/main/java/com/example/btandroid/BtAndroid.java
+++ btandroid/src/main/java/com/example/btandroid/BtAndroid.java
@@ -2,7 +2,8 @@ package com.example.btandroid;
 
 import android.util.Log;
 
-public class MainActivity {
+public class BtAndroid {
+
     
     public void testing(){
         Log.i("Testing", "function testing");
