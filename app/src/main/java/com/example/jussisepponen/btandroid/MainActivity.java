43d47457e425193e8990931168c44934014ca266
Jussi Sepponen <jussi.sepponen@rdvelho.com>
2018-07-25 15:50:50 +0300

    added ble scanner

--- app/src/main/java/com/example/jussisepponen/btandroid/MainActivity.java
+++ app/src/main/java/com/example/jussisepponen/btandroid/MainActivity.java
@@ -1,10 +1,25 @@
 package com.example.jussisepponen.btandroid;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
+
+import com.example.btandroid.BtAndroid;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private BluetoothAdapter mBluetoothAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,12 +27,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.i("MainActivity", "testing");
+        permissions();
+
+        BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+
+        BtAndroid btAndroid = new BtAndroid(mBluetoothAdapter);
+        btAndroid.scan();
     }
 
-    public void testing(){
-        Log.i("Testing", "function testing");
-    }
+    private void permissions(){
 
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)){
+                Toast.makeText(this, "The permission to get BLE location data is required", Toast.LENGTH_SHORT).show();
+            }else{
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                }
+            }
+        }else{
+            Toast.makeText(this, "Location permissions already granted", Toast.LENGTH_SHORT).show();
+        }
+    }
 
 
 }
--- btandroid/src/main/java/com/example/btandroid/BtAndroid.java
+++ btandroid/src/main/java/com/example/btandroid/BtAndroid.java
@@ -1,5 +1,8 @@
 package com.example.btandroid;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.os.Handler;
 import android.util.Log;
 import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
@@ -10,27 +13,69 @@ import java.util.List;
 
 public class BtAndroid {
 
-    public static void test(String message){
-        Log.i("Testing", message);
+    private BluetoothAdapter mBluetoothAdapter;
+
+    public BtAndroid(BluetoothAdapter mBluetoothAdapter) {
+        this.mBluetoothAdapter = mBluetoothAdapter;
     }
 
-    public void BleScan(){
+    private Handler mHandler = new Handler();
+    private boolean mScanning = true;
+    final boolean enable = true;
 
-            ScanCallback mLeScanCallback = new ScanCallback() {
-            @Override
-            public void onScanResult(int callbackType, ScanResult result) {
-                super.onScanResult(callbackType, result);
-            }
+    private final static long SCAN_PERIOD = 10000;
 
-            @Override
-            public void onBatchScanResults(List<ScanResult> results) {
-                super.onBatchScanResults(results);
-            }
+    private ScanCallback mLeScanCallback = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            super.onScanResult(callbackType, result);
 
-            @Override
-            public void onScanFailed(int errorCode) {
-                super.onScanFailed(errorCode);
-            }
-        };
+            BluetoothDevice bluetoothDevice = result.getDevice();
+            ScanRecord scanRecord = result.getScanRecord();
+            String deviceAddress = bluetoothDevice.getAddress();
+            int rssi = result.getRssi();
+
+        }
+
+        @Override
+        public void onBatchScanResults(List<ScanResult> results) {
+            super.onBatchScanResults(results);
+            Log.i("results", results.toString());
+
+        }
+
+        @Override
+        public void onScanFailed(int errorCode) {
+            super.onScanFailed(errorCode);
+
+        }
+    };
+
+    public void scanLeDevice(final boolean enable) {
+
+        final BluetoothLeScanner bluetoothLeScanner = mBluetoothAdapter.getBluetoothLeScanner();
+
+        if (enable) {
+
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mScanning = false;
+
+                    bluetoothLeScanner.stopScan(mLeScanCallback);
+                }
+            }, SCAN_PERIOD);
+
+            mScanning = true;
+            bluetoothLeScanner.startScan(mLeScanCallback);
+        } else {
+            mScanning = false;
+            bluetoothLeScanner.stopScan(mLeScanCallback);
+        }
     }
+
+    public void scan(){
+        scanLeDevice(enable);
+    }
+
 }
